0d16e4ac11e95931f9d1f95027d8008e83e7386c
HelloWorld <sarojcrest45@gmail.com>
2026-07-25 23:43:52 +0530

    Time: 1 ms (100.00%) | Memory: 120.2 MB (8.52%) - LeetSync

---
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode first=head;
+        for(int i=1;i<k;i++){
+            first=first.next;
+        }
+        ListNode kfromstart=first;
+        ListNode second=head;
+        while(first.next!=null){
+            first=first.next;
+            second=second.next;
+        }
+        ListNode kfromend=second;
+        int temp=kfromstart.val;
+        kfromstart.val=kfromend.val;
+        kfromend.val=temp;
+        return head;
+
+    }
+}
+/*
+Approach:
+- First, move a pointer to the kth node from the beginning and keep its reference.
+- Then, start another pointer from the head and move both pointers together.
+- When the first pointer reaches the last node, the second pointer will automatically be at the kth node from the end.
+- Finally, swap the values of these two nodes and return the head.
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
